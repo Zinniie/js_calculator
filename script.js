d87b84eefe03193c2cf6cad053292ddd94983468
Zinniie <ezinne.blessing@yahoo.com>
2022-06-22 22:30:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let operations={
+    '+':"Addition",
+    '-':"Subtraction",
+    "/":"Divison",
+    '*':"Multiplication"
+}
+function calculator(){
+    let operand1= Number(prompt("Enter a Number"))
+    let operation = prompt("What Operations Would You Like To Perform [+,-,/,*]")
+    let operand2= Number(prompt("Enter a Number"))
+    let result=0
+    if (isNaN(operand1) || isNaN(operand2)) {
+        throw   new Error("Alphabets  Not allowed Only Number")
+    }
+    
+    else{
+        parseFloat(operand1)
+        parseFloat(operand2)
+        if(operation ==="+"){
+            result=operand1+operand2
+        }
+        else if(operation ==="-"){
+            result=operand1-operand2
+        }
+        else if(operation ==="/"){
+            result= operand1 /operand2
+        }
+        else if(operation ==="*"){
+            result=operand1*operand2
+        }
+        else{
+            alert("invalid Operation Cannot Perform operation")
+        }
+
+    }      
+    alert(`${result}Your operation: ${operations[operation]}\n The Result is ${result.toFixed(2)}`)
+}
+try{
+  calculator()
+}
+catch(err){
+    alert(err)
+}
